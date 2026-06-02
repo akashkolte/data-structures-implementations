0a81d22b80cd394ee7ae38db04ab34f5c936feaf
Akash Kolte <kolteakash07@gmail.com>
2026-06-02 17:06:55 -0400

    added skeleton code

--- trie/trie.py
+++ trie/trie.py
@@ -1,6 +1,54 @@
 # TODO
-# •    insert(word)
-# •    search(word)
-# •    starts_with(prefix)
-# •    delete(word)
-# •    autocomplete(prefix)
+
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        pass
+    
+    def search(self, word):
+        pass
+    
+    def delete(self, word):
+        pass
+    
+    def has_prefix(self, prefix):
+        pass
+    
+    def starts_with(self, prefix):
+        pass
+    
+    def list_words(self):
+        pass
+    
+    def autocomplete(self, prefix):
+        pass
+
+if __name__ == "__main__":
+    trie = Trie()
+
+    trie.insert('hello')
+    trie.insert('henry')
+    trie.insert('mike')
+    trie.insert('minimal')
+    trie.insert('minimum')
+    trie.insert('akash')
+
+    print(trie.list_words())
+    print(trie.has_prefix('mi'))
+    print(trie.starts_with('mi'))
+    trie.delete('minimal')
+    print(trie.starts_with('mi'))
+
+    print(trie.search('minimum'))
+    print(trie.search('minimal'))
+    print(trie.search('mini'))
+
+    trie.insert('mini')
+    print(trie.starts_with('mi'))
